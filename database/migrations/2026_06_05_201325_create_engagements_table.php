e72d1ab01e4c3bd08b7a276a6e25d5a8e5144948
Andrew Emad <andrewemad@Andrew.local>
2026-06-06 10:51:55 +0300

    initial setup

---
+++ database/migrations/2026_06_05_201325_create_engagements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('engagements', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('cohort_id')->constrained('cohorts')->onDelete('cascade');
+            // $table->foreignId('instructor_id')->constrained('users')->onDelete('cascade');
+            $table->enum('type', ['lecture', 'lab', 'business_session']);
+            $table->timestamp('starts_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->decimal('scheduled_hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('engagements');
+    }
+};
