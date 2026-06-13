86b8ef6a5c008c1ac0c9f9dbd09dacd1fcba4776
mohamedabdelhaq-123 <mohamed.abdelhaq99@gmail.com>
2026-06-13 04:35:42 +0300

    style: fix naming and formatting throughout Engagement model and API resources

--- app/Http/Resources/ExcuseRequestResource.php
+++ app/Http/Resources/ExcuseRequestResource.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ExcuseRequestResource extends JsonResource
 {
-    public function toArray (Request $request): array
+    public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
@@ -26,7 +26,7 @@ class ExcuseRequestResource extends JsonResource
             'reviewed_at' => $this->reviewed_at?->toISOString(),
             'created_at' => $this->created_at?->toISOString(),
             'updated_at' => $this->updated_at?->toISOString(),
-            'attachment_url' => $this->attachment_path ? asset('storage/' . $this->attachment_path) : null,
+            'attachment_url' => $this->attachment_path ? asset('storage/'.$this->attachment_path) : null,
         ];
     }
 }
